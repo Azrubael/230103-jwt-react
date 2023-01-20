42bee4986ed0c384877489a3860b3e9f401605f2
Azrubael <i0638464000@gmail.com>
2023-01-20 12:50:49 +0200

    Step #05. Validation ant getting all users. Movie time 01:06:40

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -35,7 +35,10 @@ class UserController {
 
    async logout(req, res, next) {
       try {
-
+         const { refreshToken } = req.cookies
+         const token = await userService.logout(refreshToken)
+         res.clearCookie('refreshToken')
+         return res.json(token)
       } catch(e) {
          next(e)
       }
@@ -55,7 +58,11 @@ class UserController {
 
    async refresh(req, res, next) {
       try {
-
+         const { refreshToken } = req.cookies
+         const userData = await userService.refresh(refreshToken)
+         res.cookie('refreshToken', userData.refreshToken,
+            {maxAge: 30*24*60*60*1000, httpOnly: true})
+         return res.json(userData)         
       } catch(e) {
          next(e)
       }
@@ -63,7 +70,8 @@ class UserController {
 
    async getUsers(req, res, next) {
       try {
-         res.json(['777', '999'])
+         const users = await userService.getAllUsers()
+         return res.json(users)
       } catch(e) {
          next(e)
       }
